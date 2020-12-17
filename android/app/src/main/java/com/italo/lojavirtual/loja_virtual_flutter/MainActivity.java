a251284acff0eac7569aa965ca7102f2590da242
italoRodri021 <orodrigues.19992018@gmail.com>
2020-12-17 16:25:03 -0300

    version code 1 - 1.1.1

---
+++ android/app/src/main/java/com/italo/lojavirtual/loja_virtual_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.italo.lojavirtual.loja_virtual_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual_flutter/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'YouStore',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141)
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen()
+    );
+  }
+}
+
